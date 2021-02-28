e5793a33a1829496c518033d000dd32dbf93213a
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2021-02-27 22:29:28 -0300

    countdown button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,16 @@ import Head from 'next/head';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { CountDown } from '../components/CountDown';
 
 import styles from '../style/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className = {styles.container} >
+        <Head>
+            <title>Inicio | move.it</title>
+        </Head>
 
         <ExperienceBar/>
 
@@ -16,6 +20,7 @@ export default function Home() {
           <div>
              <Profile/>
              <CompletedChallenges/>
+             <CountDown/>
           </div>
 
           <div>
